fd865d5416763358e011ea64fd352ffed96cda62
Mohit Verma <113125771+MohitVerma2002@users.noreply.github.com>
2022-12-17 16:05:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let string = "";
+let audiopress = new Audio('beep.mp3');
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        audiopress.play();
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = " ";
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
